d731c51458b197a17ba243eb6bc69ee9d2c8a382
Haidar Alessa <10682@holbertonstudents.com>
2025-04-11 17:25:49 +0000

    Fix issues compatibility of betty style.

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,22 +1,21 @@
 #include <stdio.h>
-#include "sort.h"
 
 /**
-* bubble_sort - sorts an array of integers in
-* ascending order using the Bubble Sort algorithm.
+* bubble_sort - sorts an array of integers
+* in ascending order using the Bubble Sort algorithm.
 * @array: a pointer to the array of integers.
 * @size: the size of the array.
 */
 void bubble_sort(int *array, size_t size)
 {
-	int temp;
-	int swapped;
+	int temp, swapped;
+	size_t i, j, k;
 
-	for (size_t i = 0; i < size - 1; i++)
+	for (i = 0; i < size - 1; i++)
 	{
 		swapped = 0;
 
-		for (size_t j = 0; j < size - i - 1; j++)
+		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
@@ -24,7 +23,7 @@ void bubble_sort(int *array, size_t size)
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
 
-				for (size_t k = 0; k < size; k++)
+				for (k = 0; k < size; k++)
 				{
 					printf("%d", array[k]);
 					if (k < size - 1)

214324cb0262d6ef64e525d8e3b8d8dc59103efa
Miad008 <10694@holbertonstudents.com>
2025-04-12 09:33:32 +0000

    Add selection_sort function to sort array using Selection Sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+#include <stdio.h>
+
+/**
+* selection_sort-Sorts an array of int in ascending order using the Selection sort
+* @array: Pointer to the array to be sorted
+* @size: Size of the array
+*/
+void selection_sort(int *array, size_t size)
+{
+size_t i, j, min_index;
+int temp;
+
+if (!array || size < 2)
+return;
+
+for (i = 0; i < size - 1; i++)
+{
+min_index = i;
+for (j = i + 1; j < size; j++)
+{
+if (array[j] < array[min_index])
+min_index = j;
+}
+
+if (min_index != i)
+{
+temp = array[i];
+array[i] = array[min_index];
+array[min_index] = temp;
+
+print_array(array, size);
+}
+}
+}
